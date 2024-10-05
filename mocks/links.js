779203019978b0d6d54e43e598ffc1c56a1e4ee0
Markaz9in <152800155+Markaz9in@users.noreply.github.com>
2024-10-05 21:27:12 +0300

    Add files via upload

---
+++ mocks/links.js
@@ -0,0 +1,44 @@
+export const links = [
+  {
+    title: "Neon54-DE",
+    link: "https://weplaywithme.com/rDLTMR?",
+  },
+  {
+    title: "SlotsPalace-DE",
+    link: "https://weplaywithme.com/c8BvNh?",
+  },
+
+  {
+    title: "Bankonbet-DE",
+    link: "https://weplaywithme.com/QXkfnZ?",
+  },
+  {
+    title: "VerdeCasino-DE",
+    link: "https://weplaywithme.com/vh2xdT?",
+  },
+  {
+    title: "BitcoinBank-DE",
+    link: "https://uhfb362b70uh.ggljjk.cc/?target=-7EBNQCgQAAAdXMAIDmIwABQEBEREKEQkKEQ1CEQ0SAAF_YWRjb21ibwEx&subacc=1",
+  },
+  {
+    title: "BitCod-DE",
+    link: "https://weplaywithme.com/1wqJnF?",
+  },
+  {
+    title: "VegasPlus-ES",
+    link: "https://weplaywithme.com/gMghVP?",
+  },
+  {
+    title: "UniqueCasino-ES",
+    link: "https://weplaywithme.com/TJkvVh?",
+  },
+  {
+    title: "MaChance-ES",
+    link: "https://weplaywithme.com/sxhBy1?",
+  },
+  {
+    title: "PlayRegal-ES",
+    link: "https://weplaywithme.com/nrh9dg?",
+  },
+  
+];
---
+++ mocks/users.js
@@ -0,0 +1,3 @@
+export const users = [
+  { id: 466052008, name: "vitaliyCh" },
+];
